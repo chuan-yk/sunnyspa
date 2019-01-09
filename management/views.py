830cf8233a71d09373306c4099ae1a04c93632e1
dendi <d2265327166@gmail.com>
2019-01-09 15:07:04 +0800

    首页

--- management/views.py
+++ management/views.py
@@ -9,8 +9,7 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models import F
-from django.db.models import Sum
-
+from django.db.models import Avg, Count, Min, Sum
 
 from .models import Massage
 from .models import ServiceMenu
@@ -24,7 +23,8 @@ def index(request):
     current_year = timezone.now().year
     current_month = timezone.now().month
     orders = Massage.objects.filter(service_date__year=current_year, service_date__month=current_month)
-    orders_count = orders.count()
-    orders_sum =
-    loger.debug('current month {} have {} orders '.format(current_month, orders.count()))
-    return render(request, 'index.html', {'orders': orders})
+    content = orders.aggregate(Sum('amount'), Count('pk'), )
+    person_list = orders.values('phone', 'name').annotate(Count('phone'))
+    person_count = len(person_list)
+    loger.debug('current month {} have {} orders, toal amount = {}  '.format(current_month, content['pk__count'], content['amount__sum']))
+    return render(request, 'index.html', {'content': content, 'person_count': person_count})
